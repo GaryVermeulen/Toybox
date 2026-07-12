cfd4fa281860c9a04c6ffb374a725d8dd41e0a05
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2026-07-12 14:13:05 -0700

    Add files via upload

--- NeuralNetworksFromScratch/nnClasses1.py
+++ NeuralNetworksFromScratch/nnClasses1.py
@@ -77,6 +77,92 @@ class Activation_Softmax:
             # Calculate sample-wise gradient and
             # add it to the array of sample gradients
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Optimizer_SGD:
+
+    # Initialize optimizer - set settings, learning rate of 1.0 is default for this optimizer
+    def __init__(self, learning_rate = 1.0, decay = 0.0, momentum = 0.0):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = momentum
+
+    def pre_update_params(self):
+        # Call once before any parametr updates
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        # Update parameters
+
+        # If we use momentum
+        if self.momentum:
+            # If layer does not contain momentum arrays, create them filled with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights, the array for biases doesn't yet exist too.
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Build weight updates with momentum - take perious updates mu;tipled by retain factor and
+            # update with current gradients
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        else:
+            # Vanilla SGD updates (as before momentum update)
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        # Update weights and biases using either vanilla or momentum updates
+        layer.weights += weight_updates
+        layer.biases += bias_updates
+
+    def post_update_params(self):
+        # Call once after any parameter updates
+        self.iterations += 1
+
+
+class Optimizer_Adagrad:
+
+    # Initialize and set vars
+    def __init__(self, learning_rate = 1., decay = 0., epsilon = 1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    def pre_update_params(self):
+        # Call once before any parameter updates
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decy * self.iterations))
+
+    def update_params(self, layer):
+
+        # if layer doesn't contain cache arrays, create them filled w/zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update cache with squared current gradients
+        layer.weight_cache += layer.dweights**2
+        layer.bias_cache += layer.dbiases**2
+
+        # Vanilla SGD parameter update + normization w/square rooted cache
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        # Call oncew after any parameter updates
+        self.iterations += 1
+        
+        
             
 
 class Loss:
@@ -125,7 +211,7 @@ class Loss_CategoricalCrossentropy(Loss):
         # Backward pass
 
         # Number of samples
-        sample = len(dvalues)
+        samples = len(dvalues)
 
         # Number of labels in every sample
         # We'll use the first sample to count them
